207dee2cc95bc8b3e3cef098531f96f46365e673
Natalia <nataxawolf@gmail.com>
2023-09-22 13:46:53 +0300

    #4

--- Seminars/test2.py
+++ Seminars/test2.py
@@ -1,10 +1,17 @@
-import sys
+# import sys
+#
+# t = "123445567"
+# print(str.isdecimal(t))
+# # print(bin(t))
+# # print(oct(t))
+# # print(hex(t))
+#
+# print(str.isascii(t))
+# print(str.isalnum(t))
 
-t = "123445567"
-print(str.isdecimal(t))
-# print(bin(t))
-# print(oct(t))
-# print(hex(t))
-
-print(str.isascii(t))
-print(str.isalnum(t))
+data = [25, -42, 146, 73, -100, 12]
+print(list(map(str, data)))
+# переделывает применяем - ко всем числам в списке и возвращаем макс
+print(max(data, key=lambda x: -x))
+#х - ключ-значение, убираем даденр методы
+print(*filter(lambda x: not x[0].startswith('__'),globals().items()))
