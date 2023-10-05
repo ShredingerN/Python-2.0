03b8cf19275d2c819677641ed65aa9d4de931ac3
Natalia <nataxawolf@gmail.com>
2023-10-05 12:34:56 +0300

    #7

--- Seminars/test2.py
+++ Seminars/test2.py
@@ -7,11 +7,26 @@
 # # print(hex(t))
 #
 # print(str.isascii(t))
-# print(str.isalnum(t))
+# # print(str.isalnum(t))
+#
+# data = [25, -42, 146, 73, -100, 12]
+# print(list(map(str, data)))
+# # переделывает применяем - ко всем числам в списке и возвращаем макс
+# print(max(data, key=lambda x: -x))
+# # х - ключ-значение, убираем даденр методы
+# print(*filter(lambda x: not x[0].startswith('__'), globals().items()))
 
-data = [25, -42, 146, 73, -100, 12]
-print(list(map(str, data)))
-# переделывает применяем - ко всем числам в списке и возвращаем макс
-print(max(data, key=lambda x: -x))
-#х - ключ-значение, убираем даденр методы
-print(*filter(lambda x: not x[0].startswith('__'),globals().items()))
+import json
+#
+# with open('user.json', 'r', encoding='utf-8') as f:
+#     json_file = json.load(f)
+#
+# print(f'{type(json_file) = }\n{json_file = }')
+# print(f'{json_file["name"] = }')
+# print(f'{json_file["address"]["geo"] = }')
+# print(f'{json_file["email"] = }')
+
+a = 'Hello world!'
+b = {key: value for key, value in enumerate(a)}
+c = json.dumps(b, indent=3, separators=('; ', '= '))
+print(c)
